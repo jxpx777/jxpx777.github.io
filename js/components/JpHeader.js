1e1ea19c64cb541b7aeb2b11056c76186c06d7a5
Jamie Phelps <jamie@jamiephelps.com>
2025-05-26 13:45:12 -0700

    JpHeader - Markup cleanup and indentation fix

--- js/components/JpHeader.js
+++ js/components/JpHeader.js
@@ -6,33 +6,22 @@ class JpHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
 <header>
-        <h1><a href="index.html">Elevation</a></h1>
-        <nav id="nav">
-          <ul>
-            <li><a href="index.html">Home</a></li>
-            <li>
-              <a href="#" class="icon solid fa-angle-down">Page Layouts</a>
-              <ul>
-                <li><a href="left-sidebar.html">Left Sidebar</a></li>
-                <li><a href="right-sidebar.html">Right Sidebar</a></li>
-                <li><a href="no-sidebar.html">No Sidebar</a></li>
-                <li>
-                  <a href="#">Submenu</a>
-                  <ul>
-                    <li><a href="#">Option 1</a></li>
-                    <li><a href="#">Option 2</a></li>
-                    <li><a href="#">Option 3</a></li>
-                    <li><a href="#">Option 4</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li><a href="elements.html">Elements</a></li>
-          </ul>
-        </nav>
-      </header>
-
-		`;
+  <h1><a href="index.html">Jamie Phelps</a></h1>
+  <nav id="nav">
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li>
+        <a href="#" class="icon solid fa-angle-down">Page Layouts</a>
+        <ul>
+          <li><a href="left-sidebar.html">Left Sidebar</a></li>
+          <li><a href="right-sidebar.html">Right Sidebar</a></li>
+          <li><a href="no-sidebar.html">No Sidebar</a></li>
+        </ul>
+      </li>
+      <li><a href="elements.html">Elements</a></li>
+    </ul>
+  </nav>
+</header>`;
   }
 }
 
