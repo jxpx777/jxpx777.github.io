3006edad01a9ff39e930c44f5c0689f6db32bcd4
Jamie Phelps <jamie@jamiephelps.com>
2025-07-12 19:29:30 -0700

    Use <menu> tag for nav

--- js/components/JpNav.js
+++ js/components/JpNav.js
@@ -8,14 +8,14 @@ class JpNav extends HTMLElement {
 
     this.innerHTML = `
   <nav>
-    <ul>
+    <menu>
       <li><a href="/blog">Blog</a></li>
       <li>
 			<a href="/projects">Projects</a>
 			</li>
       <li><a href="/now">Now</a></li>
       <li><a href="/about">About</a></li>
-    </ul>
+    </menu>
   </nav>`;
   }
 }
